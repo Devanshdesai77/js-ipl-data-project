9790f52af42a3f9fc09070501128a18454a0e080
Devansh <devansh.desai.26.2@mountblue.tech>
2023-11-29 14:13:52 +0530

    Changes in matches per year.test.js

--- src/server/jestTest/matchesPerYear.test.js
+++ src/server/jestTest/matchesPerYear.test.js
@@ -1,12 +1,12 @@
 const calculateMatchesPerYear=require('../matchesPerYear')
 
 test('Passing usercase Test',() => {
-    testObj=[
+    const matches=[
         {season:'2011',id:'1'},
         {season:'2011',id:'2'},
         {season:'2011',id:'3'},
         {season:'2012',id:'4'}
     ]
-    const result = calculateMatchesPerYear(testObj);
+    const result = calculateMatchesPerYear(matches);
     expect(result).toEqual({'2011':3,'2012':1})
 }) 
