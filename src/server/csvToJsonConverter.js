16c3ae64aff9e0afe5e8d9bace2ff1cc2f4e6625
Devansh <devansh.desai.26.2@mountblue.tech>
2023-11-29 12:37:45 +0530

    Added save to json function

--- src/server/csvToJsonConverter.js
+++ src/server/csvToJsonConverter.js
@@ -2,32 +2,49 @@ const fs = require('fs')
 const path = require('path')
 const csvToJson = require('csvtojson')
 
-function convertCsvToJson(csvFileName, callback) {
-  const csvFilePath = path.join(__dirname, '..', 'data', `${csvFileName}.csv`)
+function convertCsvToJson(csvFileName, outputFilePath, callback) {
+    const csvFilePath = path.join(__dirname, '..', 'data', `${csvFileName}.csv`)
 
-  if (!fs.existsSync(csvFilePath)) {
-    const error = `Error: File does not exist. Check to make sure the file path to your csv is correct: ${csvFilePath}`;
-    console.error(error)
-    callback(error, null)
-    return;
-  }
+    if (!fs.existsSync(csvFilePath)) {
+        const error = `Error: File does not exist.${csvFilePath}`
+        console.error(error)
+        callback(error, null)
+        return
+    }
 
-  csvToJson()
-    .fromFile(csvFilePath)
-    .then((jsonArray) => {
-      callback(null, jsonArray)
-    })
-    .catch((error) => {
-      console.error(`Error converting ${csvFilePath} to JSON:`, error)
-      callback(error, null)
-    })
+    csvToJson()
+        .fromFile(csvFilePath)
+        .then((jsonArray) => {
+            fs.writeFile(outputFilePath, JSON.stringify(jsonArray, null, 2), (err) => {
+                if (err) {
+                    console.error(`Error writing JSON file ${outputFilePath}:`, err)
+                    callback(err, null)
+                } else {
+                    console.log(`Data saved to ${outputFilePath}`)
+                    callback(null, jsonArray)
+                }
+            })
+        })
+        .catch((error) => {
+            console.error(`Error converting ${csvFilePath} to JSON:`, error)
+            callback(error, null)
+        })
 }
 
-// Export datasets
-module.exports = {
-  convertCsvToJson,
-}
 
+const matchesOutputFilePath = './src/data/matches.json'
+const deliveriesOutputFilePath = './src/data/deliveries.json'
 
+convertCsvToJson('matches', matchesOutputFilePath, (errorMatches, dataMatches) => {
+    if (errorMatches) {
+       console.error(errorMatches)
+    }
+})
+
+convertCsvToJson('deliveries', deliveriesOutputFilePath, (errorDeliveries, dataDeliveries) => {
+    if(errorDeliveries){
+        console.error(errorDeliveries)
+    }
+})
 
 
